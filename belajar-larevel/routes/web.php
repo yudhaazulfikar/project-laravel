5c40e1d200048b3f0c38032d395200b0e56f010a
yudha zulfikar a <yudazulfikar19@gmail.com>
2020-09-13 10:53:34 +0700

    Controller

--- belajar-larevel/routes/web.php
+++ belajar-larevel/routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// closure
+    Route:: get('/profil', function(){
+        return " ini adalah profil siswa";
+    })->name('profil');    
+//Route name
+    Route:: get("/tesname", function(){
+        return route('profil');
+    });
+//Parameter
+    Route::get('/profil/{id}', function($id){
+        return $id;
+    });
+//Cotroller
+    Route::get("/tescontroller", "TesController@show");
+//Resource
+    Route::resource('siswa', "SiswaController");
